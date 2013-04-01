b52f43c44e1224ec2512ed9effbb2b222f76cabc
Paweł Stawicki <pawelstawicki@gmail.com>
2013-04-01 15:02:57 +0200

    /send GET and POST, with access to mongo

--- src/sample.groovy
+++ src/sample.groovy
@@ -1,14 +1,28 @@
+import com.mongodb.Mongo
+import com.mongodb.MongoURI
+
 set 'port', Integer.parseInt(System.getProperty("ratpack.port"))
 
 get("/") {
-	def ua = headers['user-agent']
-    "Your user-agent: ${ua}"
+  def ua = headers['user-agent']
+  "Your user-agent: ${ua}"
 }
     
 get("/foo/:name") {
-	"Hello, ${urlparams.name}"
+  "Hello, ${urlparams.name}"
 }
-    
-get("/person/:id") {
-	"Person #${urlparams.id}"
+
+get("/send") {
+  "Enter password for sending mailing:"
+  "<form action=\"/send\">"
+  "<input type=\"password\" name=\"password\" />"
+  "<input type=\"submit\" />"
+  "<form>"
+}
+
+post("/send") {
+  String password = params.get("password")
+  Mongo mongo = new Mongo(new MongoURI("mongodb://devcrowd:" + password + "@alex.mongohq.com:10052/app13411678"))
+  db = mongo.getDB("app13411678");
+  db.getCollection("participants")
 }
